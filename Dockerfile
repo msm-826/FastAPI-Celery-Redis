bca611082b94f510aef38ec689d6fdcbd8b663c9
msm-826 <danishmuhammed01@gmail.com>
2025-04-02 18:27:52 +0000

    change python3.12-slim to python3.12-alpine, use celery signals

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
-FROM python:3.12-slim
+FROM python:3.12-alpine
 
 WORKDIR /code
 
 ENV PYTHONUNBUFFERED=1
 ENV PYTHONDONTWRITEBYTECODE=1
 
-RUN apt-get update \
-    && apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false \
-    && rm -rf /var/lib/apt/lists/*
+RUN apk update \
+    && apk upgrade -a \
+    && rm -rf /var/cache/apk/*
 
 COPY ./requirements.txt code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r code/requirements.txt
--- app/celery_worker.py
+++ app/celery_worker.py
@@ -3,14 +3,24 @@ from datetime import datetime, timedelta
 from random import randint
 
 from celery import Celery
+from celery.signals import celeryd_after_setup, worker_init
+from celery.utils.log import get_task_logger
 
-from app.main import app
+logger = get_task_logger(__name__)
 
 celery_app = Celery(
     __name__,
     broker='redis://localhost:6379/0',
-    backend='redis://localhost:6379/0'
+    backend='redis://localhost:6379/0',
+    broker_connection_retry_on_startup=True
 )
+
+
+@celeryd_after_setup.connect
+def setup_worker(sender=None, instance=None, **kwargs):
+    logger.info("---------------------------------------")
+    logger.info(f"Worker {instance.hostname} initialized")
+    logger.info("---------------------------------------")
 
 
 @celery_app.task(name="scheduled_function")
@@ -28,4 +38,4 @@ celery_app.conf.beat_schedule = {
     }
 }
 
-celery_app.conf.timezone = 'UTC'
+celery_app.conf.timezone = 'Asia/Kolkata'
